4891659dad062b5529ca5540d34f5d4d735ba9a7
Carrie Kam <satoshiminamisama@gmail.com>
2022-07-13 23:37:46 -0400

    remove curl-test

--- curl-test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-curl --request POST http://localhost:5000/api/timeline_post -d 'name=Carrie&email=carriekam@protonmail.com&content=Just added database to my portfolio!'
-
-curl http://localhost:5000/api/timeline_post
